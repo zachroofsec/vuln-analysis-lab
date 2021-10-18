5d80dca822a1d2a325c6c97e3b20245efa813ba8
zachroofsec <zroof85@gmail.com>
2021-10-18 05:53:30 -0400

    First

--- scenario_1.sh
+++ scenario_1.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 
-echo "Process will take ~15 seconds to complete"
-
 ssh -o StrictHostKeyChecking=no \
     -o UserKnownHostsFile=/dev/null \
      ubuntu@siem-log-collector.lab <<'ENDSSH'
 sudo shutdown now > /dev/null
 ENDSSH
-
-sleep 15
-echo "Process finished!"
